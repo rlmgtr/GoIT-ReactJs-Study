053b0e6d88315142960675c19ea1d01253c8f86b
rlmgtr <rlmagtira@gmail.com>
2024-06-11 12:57:01 +0800

    worked

---
+++ studyreactjs/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+    <footer>
+      <h1>this is a sample footer</h1>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ studyreactjs/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    <header>
+      <h1>this is a sample header </h1>
+    </header>
+    </div>
+ 
+  )
+}
+
+export default Header
