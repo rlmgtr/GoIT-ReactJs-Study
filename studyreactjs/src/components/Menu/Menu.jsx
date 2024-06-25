d90f7fdee1da7e0a363eef6102837ecdbf16e5de
rlmgtr <rlmagtira@gmail.com>
2024-06-25 19:09:58 +0800

    deleted unnecessary

---
+++ studyreactjs/src/components/Menu/Menu.jsx
@@ -0,0 +1,23 @@
+const PorkAdobo = {
+    Name: "Pork Adobo",
+    Ingridients: {
+        Meat: "500 grams Pork",
+        Condiments: "half cup of soysauce, one third cup of vinegar",
+        Spices: "half tbps pepper, half tbps salt, half tbps sugar"
+    },
+    Instruction: "Saute pork, add ingridients altogether, and boil",
+    Serving: 4,
+    Type: "Filipino"
+}
+
+const BeefSinigang = {
+    Name: "Beef Sinigang",
+    Ingridients: {
+        Meat: "500 grams Beef",
+        Condiments: "half cup of tamarind extract, one third cup of fish sauce",
+        Spices: "half tbps pepper, half tbps onion, half tbps tomato"
+    },
+    Instruction: "Saute pork, add ingridients altogether, and boil",
+    Serving: 4,
+    Type: "Filipino"
+}
