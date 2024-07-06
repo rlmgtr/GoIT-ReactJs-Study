7e700db922e57c47e82d13a6bd13da49fff8b8d6
rlmgtr <rlmagtira@gmail.com>
2024-07-06 17:52:04 +0800

    update lesson

---
+++ studyreactjs/src/components/Reviews/Reviews.jsx
@@ -0,0 +1,18 @@
+import css from './Reviews-style.css'
+
+const Reviews = () => {
+
+const state = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Reviews
