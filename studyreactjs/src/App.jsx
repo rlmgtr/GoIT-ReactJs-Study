4c556d90994b682d35718f24f1c5177e6aa8ef89
rlmgtr <rlmagtira@gmail.com>
2024-07-06 18:07:47 +0800

    Prepare component

--- studyreactjs/src/App.jsx
+++ studyreactjs/src/App.jsx
@@ -1,8 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import State from './components/EventandState/State';
-import Forms from './components/Forms/Forms';
+import Reviews from './components/Reviews/Reviews';
 
 const testbuttonn = () => {alert("this is for the test button")};
 
@@ -10,8 +9,8 @@ export const App = () => {
     return (
 <div>
 <Header/>
+<Reviews/>
 <button onClick={testbuttonn}>test button 2</button>
-<Forms/>
 <Footer/>
 </div>
 
--- studyreactjs/src/components/Reviews/Reviews.jsx
+++ studyreactjs/src/components/Reviews/Reviews.jsx
@@ -3,14 +3,30 @@ import css from './Reviews-style.css'
 const Reviews = () => {
 
 const state = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
+  good: 5,
+  neutral: 3,
+  bad: 1,
 };
 
   return (
     <div>
-      
+     <div>
+<button>Good</button>
+<button>Neutral</button>
+<button>Bad</button>
+     </div>
+
+<div>
+<br></br>
+  Good:<span>{state.good}</span>
+  <br></br>
+  Neutral:<span>{}</span>
+  <br></br>
+  Bad:<span>{}</span>
+  <br></br>
+</div>
+
+
     </div>
   )
 }
