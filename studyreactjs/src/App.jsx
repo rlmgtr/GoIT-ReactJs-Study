5b42c95a32a6d8c331f0dcfb7903b32169985686
rlmgtr <rlmagtira@gmail.com>
2024-07-06 17:41:05 +0800

    added

---
+++ studyreactjs/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import State from './components/EventandState/State';
+import Forms from './components/Forms/Forms';
+
+const testbuttonn = () => {alert("this is for the test button")};
+
+export const App = () => {
+    return (
+<div>
+<Header/>
+<button onClick={testbuttonn}>test button 2</button>
+<Forms/>
+<Footer/>
+</div>
+
+)
+};
+
+export default App;
