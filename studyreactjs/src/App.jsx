f17d26660d9cc517d93dde427f3d21ab83e3f36d
rlmgtr <rlmagtira@gmail.com>
2024-08-06 15:42:16 +0800

    update

--- studyreactjs/src/App.jsx
+++ studyreactjs/src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import DataRecipes from './Data/DataRecipes.json';
-import {RecipeList} from './components/RecipeList/RecipeList';
-import State from './components/EventandState/State';
+
 
 
 
@@ -13,9 +11,6 @@ export const App = () => {
     return (
 <div>
 <Header/>
-<button onClick={testbuttonn}>test button 2</button>
-<RecipeList DataRecipes={DataRecipes}/>
-<State/>
 <Footer/>
 </div>
 )
