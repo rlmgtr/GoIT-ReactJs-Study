4c063c6d2c71a7cf39ff32c4107e977ee34863a9
rlmgtr <rlmagtira@gmail.com>
2024-09-09 14:23:47 +0800

    update

--- studyreactjs/src/App.jsx
+++ studyreactjs/src/App.jsx
@@ -1,22 +1,14 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import DataRecipes from './Data/DataRecipes.json';
-import {RecipeList} from './components/RecipeList/RecipeList';
-import State from './components/EventandState/State';
+import UseState from "./Hooks/UseState";
 
 
 
-const testbuttonn = () => {alert("this is for the test button")};
+
+
 
 export const App = () => {
     return (
 <div>
-<Header/>
-<button onClick={testbuttonn}>test button 2</button>
-<RecipeList DataRecipes={DataRecipes}/>
-<State/>
-<Footer/>
+<UseState/>
 </div>
 )
 };
---
+++ studyreactjs/src/Hooks/UseState.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UseState = () => {
+  return (
+    <div>
+      <h1>This is the UseState Hook</h1>
+    </div>
+  )
+}
+
+export default UseState
