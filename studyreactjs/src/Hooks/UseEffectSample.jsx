f2d3594f42683b9c39f904b738066ee1b8cdf95f
rlmgtr <rlmagtira@gmail.com>
2024-09-12 15:02:54 +0800

    updates

--- studyreactjs/src/Hooks/UseEffectSample.jsx
+++ studyreactjs/src/Hooks/UseEffectSample.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const UseEffectSample = () => {
+
+const [count, setCount] = useState(0)
+
+useEffect(() => {document.title = `You clicked ${count} times`
+})
+
+
+
   return (
     <div>
       <h1>This is a UseEffectSample</h1>
+<button onClick={() => setCount(count + 1)}> Clicked {count} times </button>
+
     </div>
   )
 }
@@ -11,4 +21,4 @@ const UseEffectSample = () => {
 export default UseEffectSample
 
 
-// https://www.youtube.com/watch?v=DyPkojd1fas
+//https://www.youtube.com/watch?v=06Y6aJzTmXY&list=PLC3y8-rFHvwisvxhZ135pogtX7_Oe3Q3A&index=6
