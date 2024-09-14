ba6411d329a31c5cffb99cfc967abf8005ccad82
rlmgtr <rlmagtira@gmail.com>
2024-09-14 12:47:55 +0800

    update

--- studyreactjs/src/App.jsx
+++ studyreactjs/src/App.jsx
@@ -1,17 +1,7 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-
-
-
-
-const testbuttonn = () => {alert("this is for the test button")};
-
 export const App = () => {
     return (
 <div>
-<Header/>
-<Footer/>
+
 </div>
 )
 };
